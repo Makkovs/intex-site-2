8402259a686b45a3a686c1492c3b6a6858139b8e
unknown <kovalmax09@gmail.com>
2024-08-31 11:28:54 +0300

    Work on commidity page

--- client/src/compontents/CommodityPage/CommodityPage.tsx
+++ client/src/compontents/CommodityPage/CommodityPage.tsx
@@ -1,5 +1,8 @@
-import { ReactHTMLElement, useState } from "react";
+import { useState } from "react";
 import styles from "./commodity-page.module.scss";
+
+import novaPoshtaIcon from "../../assets/nova-poshta.png";
+import ukrPoshtaIcon from "../../assets/ukr-poshta.webp";
 
 const CommodityPage = () => {
 
@@ -12,6 +15,15 @@ const CommodityPage = () => {
             setBasketNumber(available)
         } else {
             setBasketNumber(value)
+        }
+    }
+
+    const checkButton = (
+        event: React.KeyboardEvent<HTMLInputElement>,
+        callback: (...args: any[]) => void
+    ) => {
+        if (event.key === "Enter") {
+            callback();
         }
     }
 
@@ -44,52 +56,72 @@ const CommodityPage = () => {
                 </div>
             </div>
             <div className={styles.information}>
-                <h2>Блокнот</h2>
-                <span>5 продано | 12 в наявності</span>
-                <h2 className={styles.price}>500грн</h2>
-                {basketNumber > 0
-                    ?
-                    <div className={styles.added_wrapper}>
-                        <div className={styles.added_button}>
-                            <div
-                                className={styles.piece}
-                                onClick={() => changeBasketValue(basketNumber - 1)}
-                            >
-                                -
+                <div className={styles.main_information}>
+                    <h2 className={styles.title}>Блокнот</h2>
+                    <span className={styles.rating}>
+                        <span className={styles.rating_number}>4.0</span>
+                        ★ ★ ★ ★ ☆ 
+                    </span> <br />
+                    <span className={styles.available_info}>5 продано | 12 в наявності</span> 
+                    <h2 className={styles.price}>500грн</h2>
+                    {basketNumber > 0
+                        ?
+                        <div className={styles.added_wrapper}>
+                            <div className={styles.added_button}>
+                                <div
+                                    className={styles.piece}
+                                    onClick={() => changeBasketValue(basketNumber - 1)}
+                                >
+                                    -
+                                </div>
+                                <div
+                                    className={[styles.piece, styles.added].join(" ")}
+                                    onClick={() => setInputVisible(!inputVisible)}
+                                >
+                                    {basketNumber} Додано
+                                </div>
+                                <div
+                                    className={styles.piece}
+                                    onClick={() => changeBasketValue(basketNumber + 1)}
+                                >
+                                    +
+                                </div>
                             </div>
-                            <div
-                                className={[styles.piece, styles.added].join(" ")}
-                                onClick={() => setInputVisible(!inputVisible)}
-                            >
-                                {basketNumber} Додано
-                            </div>
-                            <div
-                                className={styles.piece}
-                                onClick={() => changeBasketValue(basketNumber + 1)}
-                            >
-                                +
-                            </div>
-                        </div>
-                        {inputVisible &&
-                            <input
-                                className={styles.added_input}
-                                type="number"
-                                value={basketNumber}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
-                                    changeBasketValue(Number(e.target.value))
-                                }
-                            />
+                            {inputVisible &&
+                                <input
+                                    className={styles.added_input}
+                                    type="number"
+                                    value={basketNumber}
+                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                        changeBasketValue(Number(event.target.value))
+                                    }
+                                    onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) =>
+                                        checkButton(event, () => setInputVisible(false))
+                                    }
+                                />
 
-                        }
+                            }
+                        </div>
+                        :
+                        <div
+                            className={styles.add_button}
+                            onClick={() => setBasketNumber(1)}
+                        >
+                            Додати в кошик
+                        </div>
+                    }
+                </div>
+                <div className={styles.delivery_info}>
+                    <h3 className={styles.delivery_title}>Доставка</h3>
+                    <div className={styles.delivery_row}>
+                        <img className={styles.delivery_icon} src={novaPoshtaIcon} alt="" />
+                        <div>Нова пошта </div>
                     </div>
-                    :
-                    <button
-                        className={styles.add_button}
-                        onClick={() => setBasketNumber(1)}
-                    >
-                        Додати в кошик
-                    </button>
-                }
+                    <div className={styles.delivery_row}>
+                        <img className={styles.delivery_icon} src={ukrPoshtaIcon} alt="" />
+                        <div>Укр пошта</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
